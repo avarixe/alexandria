7e9b642b28a864df8a770ff072fc1ca6afdd1079
Joon Lee <joon.lee@ucla.edu>
2019-07-24 11:51:56 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: [
+    'standard'
+    // 'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'html'
+    // 'vuetify'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-multi-spaces': 'off',
+    'camelcase': ['warn', {
+      'properties': 'never'
+    }],
+    // allow async-await
+    'generator-star-spacing': 'off',
+
+    // 'indent': 'off',
+    // 'vue/script-indent': ['error', 2, {
+    //   'baseIndent': 1,
+    //   'switchCase': 1
+    // }],
+
+    // 'vue/singleline-html-element-content-newline': 'off',
+
+    // 'vuetify/no-deprecated-classes': 'error',
+    // 'vuetify/grid-unknown-attributes': 'warn',
+    // 'vuetify/no-legacy-grid': 'warn',
+
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+  }
+}
---
+++ nuxt.config.js
@@ -0,0 +1,93 @@
+import pkg from './package'
+import nodeExternals from 'webpack-node-externals'
+
+export default {
+  mode: 'universal',
+
+  server: {
+    host: '0.0.0.0',
+    port: '8080'
+  },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'MyFIFA Manager',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' },
+      { rel: 'stylesheet', href: 'https://cdn.materialdesignicons.com/2.8.94/css/materialdesignicons.min.css' }
+    ]
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/styles/index.scss'
+  ],
+
+  /*
+  ** Customize the progress bar color
+  */
+  // loading: '~/components/App/LoadingOverlay.vue', // { color: '#3B8070' },
+
+  devModules: [
+    '@nuxtjs/vuetify'
+  ],
+
+  vuetify: {
+    assets: false
+  },
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    // analyze: true,
+
+    babel: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }]
+      ]
+    },
+
+    transpile: [/^vuetify/],
+
+    postcss: {},
+
+    extractCSS: true,
+
+    extend (config, { isDev, isClient }) {
+      // Run ESLint on save
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+      if (process.server) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
+    }
+  }
+}
